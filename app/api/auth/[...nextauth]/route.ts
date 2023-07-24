38d5db8201c196ae638001c3929026b9e03e0697
AFei <laterfei@qq.com>
2023-07-24 18:55:58 +0800

    feat:login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,23 +11,27 @@ export const authOptions: NextAuthOptions = {
       name: "",
       credentials: {},
       async authorize(credentials, req) {
-        return {
-          name: "Xiaoming",
-          email: "1123123@qq.com",
-          avatar: "https://avatars.githubusercontent.com/u/1396951?v=4",
-        };
+        // return {
+        //   name: "Xiaoming",
+        //   email: "1123123@qq.com",
+        //   avatar: "https://avatars.githubusercontent.com/u/1396951?v=4",
+        // };
 
-        const res = await fetch("/your/endpoint", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        const user = await res.json();
-
-        if (res.ok && user) {
-          return user;
+        try {
+          const res = await fetch(Path.login, {
+            method: "POST",
+            body: JSON.stringify(credentials),
+            headers: { "Content-Type": "application/json" },
+          });
+          console.log(res);
+          const user = await res.json();
+          if (res.ok && user) {
+            return user;
+          }
+          return null;
+        } catch (error) {
+          console.log(error);
         }
-        return null;
       },
     }),
   ],
